d3d850c920f634583f240b1bdf09b14a9d5ac771
Park Byung Eun <geekdev@naver.com>
2022-11-28 15:59:01 +0900

    75: file controller 추가

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -31,6 +31,7 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)],
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] }),
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -71,5 +72,11 @@ export class PostCreateComponent implements OnInit {
     this.form.reset();
   }
 
-  onImagePicked(): void {}
+  onImagePicked(event: Event): void {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+  }
 }
