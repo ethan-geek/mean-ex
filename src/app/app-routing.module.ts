69038b6152c6e4e755747a258441dc3ae3c43fec
Park Byung Eun <geekdev@naver.com>
2022-11-30 11:41:53 +0900

    97: 회원가입 화면 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
@@ -20,6 +21,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
   },
 ];
 
---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent {
+  isLoading = false;
+
+  constructor() {}
+
+  onSignup(form: NgForm): void {
+    console.log(form);
+  }
+}
