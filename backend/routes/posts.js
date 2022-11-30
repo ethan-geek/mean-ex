843c2b693504996d9f1ec950b7fb3f2807e5b705
Park Byung Eun <geekdev@naver.com>
2022-11-30 16:36:32 +0900

    104: check jwt token 함수 추가

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const checkAuth = require("../middleware/check-auth");
 
 const Post = require("../models/post");
 
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 
 router.post(
   "",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
@@ -53,6 +55,7 @@ router.post(
 
 router.put(
   "/:id",
+  checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -114,7 +117,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   console.log(req.params.id);
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
