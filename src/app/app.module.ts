a9778b07a44e676b59161e56f3bec88cde8616ba
Park Byung Eun <geekdev@naver.com>
2022-12-02 16:48:34 +0900

    134: 앱 기능 모듈로 리팩토링

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -16,13 +16,14 @@ import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AppMaterialModule } from './app-material.module';
+import { AppPostsModule } from './posts/posts.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
+
     HeaderComponent,
-    PostListComponent,
+
     LoginComponent,
     SignupComponent,
     ErrorComponent,
@@ -31,12 +32,13 @@ import { AppMaterialModule } from './app-material.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
+
     BrowserAnimationsModule,
 
     HttpClientModule,
 
     AppMaterialModule,
+    AppPostsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppMaterialModule } from '../app-material.module';
+
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
+
+@NgModule({
+  declarations: [PostCreateComponent, PostListComponent],
+  imports: [CommonModule, ReactiveFormsModule, AppMaterialModule, RouterModule],
+})
+export class AppPostsModule {}
